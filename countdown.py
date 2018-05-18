2fa0773c0c530434a4dfe175baa07a2da1bcc8a3
Pranav Anbarasu <19339128+Pergamon256@users.noreply.github.com>
2018-05-17 23:11:18 -0500

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,23 @@
+print('Countdown started')
+
+import time
+def countdown(m,s):
+	i=m
+	j=s
+	k=0
+	while True:
+		if(j ==- 1):
+			j = 59
+			i -= 1
+		if(j > 9):
+			print(str(k)+str(i)+":"+str(j), end='\r') #Carriage return only works with python 3, end='\r' will not work with python 2
+		else:
+			print(str(k)+str(i)+":"+str(k)+str(j), end='\r')
+		time.sleep(1)
+		j -= 1
+		if(i == 0 and j == -1):
+			break
+	if(i == 0 and j == -1):
+		print('Countdown ended')
+		time.sleep(1)
+countdown(1,0)
