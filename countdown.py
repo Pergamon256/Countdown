e17a53662f06b940df2b4151ce2a893eb3552cd8
Pranav Anbarasu <19339128+Pergamon256@users.noreply.github.com>
2018-05-17 23:12:50 -0500

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,4 +1,4 @@
-print('Countdown started')
+print('Countdown started') #You can edit this to display whatever text you want, even include strings using {} and .format()
 
 import time
 def countdown(m,s):
@@ -10,7 +10,7 @@ def countdown(m,s):
 			j = 59
 			i -= 1
 		if(j > 9):
-			print(str(k)+str(i)+":"+str(j), end='\r') #Carriage return only works with python 3, end='\r' will not work with python 2
+			print(str(k)+str(i)+":"+str(j), end='\r') #Carriage return only works with python 3; end='\r' will not work with python 2.
 		else:
 			print(str(k)+str(i)+":"+str(k)+str(j), end='\r')
 		time.sleep(1)
@@ -18,6 +18,6 @@ def countdown(m,s):
 		if(i == 0 and j == -1):
 			break
 	if(i == 0 and j == -1):
-		print('Countdown ended')
+		print('Countdown ended') #You can edit this to display whatever text you want, even include strings using {} and .format()
 		time.sleep(1)
 countdown(1,0)
